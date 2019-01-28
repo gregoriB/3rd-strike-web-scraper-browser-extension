3b247701fdec6b054d8366a4812a466f9ae5cdc8
gregoriB <brandon.gregori@gmail.com>
2019-01-27 22:13:54 -0800

    updated code to work with the "misc" category. Made it not work with incompatible categories, such as the move list

--- scripts.js
+++ scripts.js
@@ -1,29 +1,35 @@
-const button   = document.querySelectorAll('li'),
+const moveSet  = document.querySelectorAll('li'),
       charName = document.querySelector('#topTitle');
 
-let charData = {},
-    th       = document.querySelectorAll('th'),
-    tr       = document.querySelectorAll('tr');
+let charData = {};
 
 function buildObject(type) {
+  const th = document.querySelectorAll('th'),
+        tr = document.querySelectorAll('tr'),
+        dd = document.querySelectorAll('dd');
   charData = {}
   charData.name = charName.innerText;
   charData.category = type;
-  th = document.querySelectorAll('th');
-  tr = document.querySelectorAll('tr');
-  tr.forEach(row => {
-    const td = row.querySelectorAll('td');
-    let obj = {}
-    for (let i = 2; i < th.length; i++) if(td[i]) obj[th[i].innerText] = td[i].innerText.trim();
-    if (td[1]) charData[td[1].innerText] = obj;
-  });
+  if (dd.length > 0) {
+    dd.forEach(data => {
+      const p = data.querySelector('p');
+      charData[p.innerText] = data.innerText.split(': ')[1]; // splitting at ': ' because data.innerText pulls the text from the <p> as well
+    });
+  } else if (tr.length > 0) {
+    tr.forEach(row => {
+      const td = row.querySelectorAll('td');
+      let obj = {}
+      for (let i = 2; i < th.length; i++) if(td[i]) obj[th[i].innerText] = td[i].innerText.trim();
+      if (td[1]) charData[td[1].innerText] = obj; // push temp object into the main charData object
+    });
+  } 
 }
 
 const downloadFile = () => {
   const file   = new Blob([JSON.stringify(charData, null, '\t')], {type: 'application/json'}),
         anchor = document.createElement('a'),
         name   = charData.name.split(' ')[0] + ' ' + charData.category + '.json';
-  document.body.appendChild(anchor);
+  document.body.appendChild(anchor); // download doesn't work in firefox unless the <a> exists in the body
   anchor.style.display = 'none';
   anchor.href = URL.createObjectURL(file);
   anchor.download = name;
@@ -31,11 +37,14 @@ const downloadFile = () => {
 }
 
 function exportObject() {
+  const id = this.id;
+  if (id === 'general_basic' || id === 'general_moves') return;
+ 
   setTimeout(() => {
     let type = this.innerText;
-    buildObject(type);
+    buildObject(type);      
     downloadFile();
-  }, 1000);
+  }, 1000); // setTimeout gives the webpage time to fetch the data from the database
 }
 
-button.forEach(li => li.addEventListener('click', exportObject));
+moveSet.forEach(li => li.addEventListener('click', exportObject));
