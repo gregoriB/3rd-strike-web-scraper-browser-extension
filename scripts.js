77079dd0d571d325c822536a250b06ce9b60c22a
gregoriB <brandon.gregori@gmail.com>
2019-01-28 17:58:10 -0800

    cleaned up code a bit

--- scripts.js
+++ scripts.js
@@ -1,25 +1,30 @@
 const moveSet  = document.querySelectorAll('li'),
       charName = document.querySelector('#topTitle');
 
-let charData = {};
+let charData = {}
 
 function buildObject(type) {
-  const th = document.querySelectorAll('th'),
-        tr = document.querySelectorAll('tr'),
-        dd = document.querySelectorAll('dd');
   charData = {}
   charData.name = charName.innerText;
   charData.category = type;
+  const th = document.querySelectorAll('th'),
+        tr = document.querySelectorAll('tr'),
+        dd = document.querySelectorAll('dd');
   if (dd.length > 0) {
     dd.forEach(data => {
       const p = data.querySelector('p');
       charData[p.innerText] = data.innerText.split(': ')[1]; // splitting at ': ' because data.innerText pulls the text from the <p> as well
     });
-  } else if (tr.length > 0) {
+  } 
+  else if (tr.length > 0) {
     tr.forEach(row => {
-      const td = row.querySelectorAll('td');
-      let obj = {}
-      for (let i = 2; i < th.length; i++) if(td[i]) obj[th[i].innerText] = td[i].innerText.trim();
+      const obj = {},
+             td = row.querySelectorAll('td');
+      for (let i = 2; i < th.length; i++) {
+        if(td[i]) {
+          obj[th[i].innerText] = td[i].innerText.trim();
+        }
+      }
       if (td[1]) charData[td[1].innerText] = obj; // push temp object into the main charData object
     });
   } 
@@ -27,22 +32,20 @@ function buildObject(type) {
 
 const downloadFile = () => {
   const file   = new Blob([JSON.stringify(charData, null, '\t')], {type: 'application/json'}),
-        anchor = document.createElement('a'),
+        a      = document.createElement('a'),
         name   = charData.name.split(' ')[0] + ' ' + charData.category + '.json';
-  document.body.appendChild(anchor); // download doesn't work in firefox unless the <a> exists in the body
-  anchor.style.display = 'none';
-  anchor.href = URL.createObjectURL(file);
-  anchor.download = name;
-  anchor.click();
+  document.body.appendChild(a); // download doesn't work in firefox unless the <a> exists in the body
+  a.style.display = 'none';
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+  a.click();
 }
 
 function exportObject() {
-  const id = this.id;
-  if (id === 'general_basic' || id === 'general_moves') return;
+  if (this.id === 'general_basic' || this.id === 'general_moves') return;
  
   setTimeout(() => {
-    let type = this.innerText;
-    buildObject(type);      
+    buildObject(this.innerText);
     downloadFile();
   }, 1000); // setTimeout gives the webpage time to fetch the data from the database
 }
